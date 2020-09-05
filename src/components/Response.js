9bf5ec1b14f64503548b45439f47d748492db541
davorpa <katelme@gmail.com>
2020-09-05 12:00:23 +0200

    Fix: Support all accent vocals

--- src/components/Response.js
+++ src/components/Response.js
@@ -3,7 +3,24 @@ import React from 'react';
 class Response extends React.Component {
 
     translateText(text) {
-        const translatedText = text.replace(/[aeou]/g, 'i').replace(/[áéóú]/g, 'í').replace(/[àèòù]/g, 'ì').replace(/[AEOU]/g, 'I').replace(/qii/g, 'qui').replace(/QII/g, 'QUI').replace(/qií/g, 'quí').replace(/QIÍ/g, 'QUÍ');
+        let translatedText = text.replace(/[aeou]/g, 'i')
+                                       .replace(/[áéóú]/g, 'í')
+                                       .replace(/[àèòù]/g, 'ì')
+                                       .replace(/[äëöü]/g, 'ï')
+                                       .replace(/[âêôû]/g, 'î');
+        translatedText = translatedText.replace(/[AEOU]/g, 'I')
+                                       .replace(/[ÁÉÓÚ]/g, 'Í')
+                                       .replace(/[ÀÈÒÙ]/g, 'Ì')
+                                       .replace(/[ÄËÖÜ]/g, 'Ï')
+                                       .replace(/[ÂÊÔÛ]/g, 'Î');
+        translatedText = translatedText.replace(/qii/g, 'qui')
+                                       .replace(/QII/g, 'QUI')
+                                       .replace(/qií/g, 'quí')
+                                       .replace(/QIÍ/g, 'QUÍ')
+                                       .replace(/qiï/g, 'quï')
+                                       .replace(/QIÏ/g, 'QUÏ')
+                                       .replace(/qiî/g, 'quî')
+                                       .replace(/QIÎ/g, 'QUÎ');
 
         return translatedText;
     }
